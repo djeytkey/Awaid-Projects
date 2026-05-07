3f845f3b160165f8ebd83836acc06dd415bb7b53
Tarik BOUKJIJ <tarik.engineering@gmail.com>
2026-05-07 11:18:38 +0100

    version 1.1.0

--- awaid-projects.php
+++ awaid-projects.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Awaid Projects
  * Description: Custom post type for real estate projects with a single project layout.
- * Version: 1.0.9
+ * Version: 1.1.0
  * Author: Awaid
  * Text Domain: awaid-projects
  */
@@ -11,7 +11,7 @@ if (!defined('ABSPATH')) {
 	exit;
 }
 
-define('AWAID_PROJECTS_VERSION', '1.0.9');
+define('AWAID_PROJECTS_VERSION', '1.1.0');
 define('AWAID_PROJECTS_PATH', plugin_dir_path(__FILE__));
 define('AWAID_PROJECTS_URL', plugin_dir_url(__FILE__));
 
